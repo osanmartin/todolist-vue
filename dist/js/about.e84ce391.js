699e36442b56f74aef343e4b8cf53c799e35a263
Orlando San Martín <orlando@odd.co>
2022-07-04 14:30:46 -0400

    new deploy

---
+++ dist/js/about.e84ce391.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktodolist"]=self["webpackChunktodolist"]||[]).push([[443],{7381:function(t,n,s){s.r(n),s.d(n,{default:function(){return d}});var u=s(3396);const a={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),o=[e];function r(t,n){return(0,u.wg)(),(0,u.iD)("div",a,o)}var i=s(89);const c={},l=(0,i.Z)(c,[["render",r]]);var d=l}}]);
+//# sourceMappingURL=about.e84ce391.js.map
